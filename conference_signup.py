971f00f243bd7ae972f67c357f6e0c3de0cb1f9e
Gabriel Puliatti <gabriel@emptor.io>
2025-09-02 12:10:48 -0500

    Update conference_signup.py

--- conference_signup.py
+++ conference_signup.py
@@ -739,7 +739,7 @@ async def create_sol_call(signup: Signup):
     )
     
     # Fixed Sol's phone number
-    sol_phone = "+51981867746"
+    sol_phone = "+51922776803"
     
     # Generate room name
     participant_name = signup.name.replace(" ", "_")
